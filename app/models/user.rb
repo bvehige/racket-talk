dd404b65feece1a760614c404c760945be7b8941
Brian Vehige <brianvehige@gmail.com>
2020-12-13 15:24:13 -0800

    added users and sessions methods and correlating views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_rackets, through: :reviews, source: :racket
     has_many :rackets 
+    has_secure_password
+
+    validates :username, uniqueness: true, prescence: true
 end
