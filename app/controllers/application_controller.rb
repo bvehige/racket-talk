aad88d1d9e0d892cfb829901856bee611e409b20
Brian Vehige <brianvehige@gmail.com>
2020-12-13 23:24:59 -0800

    added a current user helper method and new review validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user:id])
+    end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,32 @@
 class ReviewsController < ApplicationController
 
-
     def new
-        racket.find_by_id(params[:id])
-        
+        @racket = Racket.find_by_id(params[:racket_id])
+        @review = @racket.reviews.build
+
     end
 
     def index
 
     end
 
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+        redirect_to review_path(@review)
+        else
+        render :new
+        end
+        
+    end
+
+    def show
+        @review = Review.find_by_id(params[:id])
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:racket_id, :content, :stars, :title)
+    end
 end
