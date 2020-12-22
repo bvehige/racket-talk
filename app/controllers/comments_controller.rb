951d1c5250fa0c6b6508008439f5de1225ce5365
Brian Vehige <brianvehige@gmail.com>
2020-12-22 00:16:19 -0800

    added basics controller actions and views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,33 @@
 class CommentsController < ApplicationController
+
+def index
+
+end
+
+def new
+
+end
+
+def create
+
+end
+
+def show
+
+end
+
+def edit
+
+end
+
+def update
+
+end
+
+private
+
+def comment_params
+    params.require(:comment).permit(:content, :review_id)
+end
+
 end
