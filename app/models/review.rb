738d48b34d34ed94b453deb72ebc2bfd41900d50
Brian Vehige <brianvehige@gmail.com>
2020-12-22 00:06:35 -0800

    updated comment, user, and review models with comment relationships

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :racket
+  has_many :comments
+  has_many :users, through: :comments
   
   validates :title, presence: true
   validates :stars, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6}
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_rackets, through: :reviews, source: :racket
     has_many :rackets #that user has created
-    
+    has_many :comments
+    has_many :commented_reviews, through: :comments, source: :reviews
+
     has_secure_password
 
     validates :username, presence: true
