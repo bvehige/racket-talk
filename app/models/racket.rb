8111525df799a6fd6216ee85bde0b9d257e7bce3
Brian Vehige <brianvehige@gmail.com>
2020-12-24 00:19:28 -0800

    added brand_attributes method to racket and changed the new view form to a datalist

--- app/models/racket.rb
+++ app/models/racket.rb
@@ -3,12 +3,17 @@ class Racket < ApplicationRecord
   belongs_to :user #who created it
   has_many :reviews
   has_many :users, through: :reviews #users who have reviewed it
-  accepts_nested_attributes_for :brand
+  #accepts_nested_attributes_for :brand
 
   validates :racket_name, presence: true
-  validates :racket_name, uniqueness: {scope: :brand, message: "has already been created for this brand"}
-  #validate :not_duplicate
+  #validates :racket_name, uniqueness: {scope: :brand, message: "has already been created for this brand"}
+  validate :not_duplicate
 
+
+  def brand_attributes=(attributes)
+    self.brand = Brand.find_or_create_by(attributes) if !attributes['name'].empty?
+    self.brand
+  end
 
   def not_duplicate
     #if brand and racket_name already exist, show an error message
