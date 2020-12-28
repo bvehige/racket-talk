73c1444f65400cb2f360de8e57c4c8aaddb34b1c
Brian Vehige <brianvehige@gmail.com>
2020-12-27 18:08:28 -0800

    changed category column name and updated category views

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2020_12_28_010927) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "type"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
